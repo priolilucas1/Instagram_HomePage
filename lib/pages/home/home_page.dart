92922fd95517be13b81d325fe82c4d0eb4e714ae
priolilucas1 <priolilucas1@gmail.com>
2021-07-08 13:45:38 -0300

    Home Page Created

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_home_page/pages/home/widgets/post_widget.dart';
+import 'package:instagram_home_page/pages/home/widgets/responsive_app_bar.dart';
+import 'package:instagram_home_page/pages/home/widgets/stories_list.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 52),
+        child: ResponsiveAppBar(),
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 1000),
+          child: ListView(
+            children: [
+              StoriesList(),
+              PostWidget(),
+              PostWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
